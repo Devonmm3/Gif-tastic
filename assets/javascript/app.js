9b9abeb08fdf0266dcbf757db46cf083608b9cca
DevonMM <Devon.m.moran@gmail.com>
2018-12-06 11:55:27 -0500

    third

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,17 +4,27 @@ function createButton(text) {
     $("#buttonsList").append("<button type='button' class='btn btn-primary buttonList'>" + text + "</button>")
 }
 
-//function that moves the gif from the opposite state that it is in (still to animated and vice versa)
-function giphySwap() {
-    $("body").on("click", "img", function () {
-        var currentSource = $(this).attr("src");
-        var altSource = $(this).attr("data-alt-src");
-        $(this).attr("src", altSource);
-        $(this).attr("data-alt-src", currentSource);
-        currentSource = $(this).attr("src");
-        altSource = $(this).attr("data-alt-src");
-    });
-};
+//function that shows the keywords from array in the buttons
 
-
+function wordToButton (){
+    for (i=0; i<topics.length; i++){
+        createButton(topics[i]);
+    }
 }
+
+//function that moves the gif from the opposite state that it is in (still to animated and vice versa)
+$("body").on("click", "img", function () {
+var state = $(this).attr("data-state");
+
+if (state === "still") {
+    $(this).attr("src", $(this.attr("data-animate"));
+    $(this).attr("data-state", "animate");
+} else {
+    $(this).attr("src", $(this).attr("data-still"));
+    $(this).attr("data-state", "still");
+}
+})
+
+
+
+
